71ae120ee288060a4bf6874790558f5032709206
vanluijt <vl54@ziggo.nl>
2022-08-08 15:01:46 +0200

    Initial Commit

---
+++ Pickermenu/PickermenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickermenuApp.swift
+//  Pickermenu
+//
+//  Created by P.A. van Luijt on 08/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct PickermenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
